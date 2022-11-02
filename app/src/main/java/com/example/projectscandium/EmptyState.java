2d6756bc2b48d58f1b91df47ced220d36c4a777f
ekw11 <ekw11@sfu.ca>
2022-11-01 18:26:58 -0700

    Added animation to empty state page
    Added images and fade animation. Also added functionality to page. And created sekeleton tutorial page

--- app/src/main/java/com/example/projectscandium/EmptyState.java
+++ app/src/main/java/com/example/projectscandium/EmptyState.java
@@ -2,13 +2,77 @@ package com.example.projectscandium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class EmptyState extends AppCompatActivity {
+
+    private static final int SECONDS = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empty_state);
     }
+
+    // Button to go to tutorial page
+    private void setupSkipButton() {
+        Button btn = (Button) findViewById(R.id.skip_button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Goes to tutorial page
+                startTutorialPage();
+
+            }
+        });
+    }
+
+    // Goes to main menu automatically 4 seconds after animation
+    private void setUpAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade);
+
+        // Set animation of image
+        ImageView img = findViewById(R.id.welcome_icon);
+        img.setAnimation(animation);
+
+        // Animation listener
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                // Starts main menu after 4 seconds
+                Handler handle = new Handler();
+                handle.postDelayed(() -> {
+
+                    // Goes to main menu
+                    startTutorialPage();
+
+                }, SECONDS);
+            }
+        });
+    }
+
+    // Goes to tutorial page activity
+    private void startTutorialPage() {
+        Intent i = new Intent(EmptyState.this, TutorialPage.class);
+        startActivity(i);
+    }
+
 }
