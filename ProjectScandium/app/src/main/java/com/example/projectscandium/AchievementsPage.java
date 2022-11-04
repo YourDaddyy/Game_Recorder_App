e6a471d5ab4d7d221ae9c128057e0ad7b84a2556
ekw11 <ekw11@sfu.ca>
2022-11-04 04:02:30 -0700

    Added functionality to achievements page
    Created buttons to update achievements page with achievement information. Note that achievement page is not dynamic and still prototype

--- ProjectScandium/app/src/main/java/com/example/projectscandium/AchievementsPage.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/AchievementsPage.java
@@ -2,8 +2,18 @@ package com.example.projectscandium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.projectscandium.model.Achievements;
+import com.example.projectscandium.model.AchievementsInterface;
+import com.example.projectscandium.model.AchievementsList;
+import com.example.projectscandium.model.AchievementsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AchievementsPage extends AppCompatActivity {
 
@@ -13,6 +23,7 @@ public class AchievementsPage extends AppCompatActivity {
         setContentView(R.layout.activity_achievements);
 
         setupBackButton();
+        setUpAchievements();
     }
 
     // Button to go to game configurations page
@@ -26,9 +37,57 @@ public class AchievementsPage extends AppCompatActivity {
         });
     }
 
+    // Get score and set up achievements
+    private void setUpAchievements() {
+
+        List<Achievements> achievements;
+
+        // Some sorta of player count is received
+        int playerCount = 3;
+        AchievementsInterface manager = new AchievementsInterface(playerCount);
+
+        // Grabs achievements from interface
+        achievements = manager.getAllAchievements();
+
+        ArrayList<Button> buttonList  = new ArrayList<Button>();
+
+        // Populates buttons array
+        buttonList.add(findViewById(R.id.achievement1));
+        buttonList.add(findViewById(R.id.achievement2));
+        buttonList.add(findViewById(R.id.achievement3));
+        buttonList.add(findViewById(R.id.achievement4));
+        buttonList.add(findViewById(R.id.achievement5));
+        buttonList.add(findViewById(R.id.achievement6));
+        buttonList.add(findViewById(R.id.achievement7));
+        buttonList.add(findViewById(R.id.achievement8));
+
+        // Sets the achievements to be displayed
+        TextView name = findViewById(R.id.achievement_name);
+        TextView rqd = findViewById(R.id.achievement_rqd);
+        TextView info = findViewById(R.id.achievement_info);
+        TextView state = findViewById(R.id.achievement_status);
+
+        // Iterates through achievements and buttons
+        for (int i = 0; i < 8; i++) {
+            int baseScore = achievements.get(i).getScore();
+            String achName = achievements.get(i).getName();
+            boolean status = achievements.get(i).isCompletion();
+
+            // Sets up buttons with achievements
+            buttonList.get(i).setOnClickListener(view -> {
+                // Updates info
+                name.setText(achName);
+                rqd.setText("Complete score of " + baseScore + " to complete!");
+                info.setText("0 / " + baseScore);
+                state.setText("Completion: " + status);
+            });
+        }
+    }
+
+
     // Goes to game configurations page activity
     private void returnToConfigPage() {
-        //Intent i = new Intent(AchievementsPage.this, gameConfigurations.class);
-        //startActivity(i);
+        Intent i = new Intent(AchievementsPage.this, TutorialPage.class);
+        startActivity(i);
     }
 }
