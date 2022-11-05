e6f9391c69f36aea1c31a70ff6d6bc5c32bb78c9
Sahaj Singh <ssa365@sfu.ca>
2022-11-05 15:06:35 -0700

    Edit Game Configuration Activity:
    Added Empty State for Game Configuration to the activity
    Added Support for Application Bar
    Connected AchievementsPage Activity to GameConfig Activity
    
    Signed-off-by: sahajsingh <ssa365@sfu.ca>

--- ProjectScandium/app/src/main/java/com/example/projectscandium/GameConfig.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/GameConfig.java
@@ -10,9 +10,12 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.projectscandium.model.ConfigManager;
 import com.example.projectscandium.model.Configs;
+
+import java.util.Objects;
 
 public class GameConfig extends AppCompatActivity {
 
@@ -20,7 +23,7 @@ public class GameConfig extends AppCompatActivity {
     ConfigManager configManager;
     Configs config;
 
-    String name, upperScore, lowerScore;
+    String name, upperScore, lowerScore, mode;
     boolean emptyName, upperScoreCheck, lowerScoreCheck;
 
     EditText nameBox, upperScoreBox, lowerScoreBox;
@@ -29,6 +32,24 @@ public class GameConfig extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_config);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        // Get intent of the activity
+        Intent intent = getIntent();
+        // Get the game mode
+        mode = intent.getStringExtra("mode");
+
+        if (Objects.equals(mode, "NewConfig")){
+            getSupportActionBar().setTitle("New Game Configuration");
+        }else if (Objects.equals(mode, "EditConfig")){
+            getSupportActionBar().setTitle("Edit Game Configuration");
+        }
+
+        // listen to up button
+        toolbar.setNavigationOnClickListener(v -> finish());
 
         emptyName = false;
         upperScoreCheck = false;
@@ -41,7 +62,6 @@ public class GameConfig extends AppCompatActivity {
         nameBox = findViewById(R.id.configNameBox);
         upperScoreBox = findViewById(R.id.UpperScoreBox);
         lowerScoreBox = findViewById(R.id.LowerScoreBox);
-
 
         // get the extra info from the intent
         int index = getIntent().getIntExtra("configIndex", -1);
@@ -211,10 +231,14 @@ public class GameConfig extends AppCompatActivity {
             }
         });
 
+        Button achievementButton = findViewById(R.id.achConfig);
+        // listener for the achievement button
+        achievementButton.setOnClickListener(view -> {
+            Intent new_intent = new Intent(this, AchievementsPage.class);
+            startActivity(new_intent);
+        });
+
     }
-
-
-
 
     private boolean checkEmpty(EditText BoxId) {
         if (BoxId.getText().toString().isEmpty()) {
--- ProjectScandium/app/src/main/java/com/example/projectscandium/GameConfigList.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/GameConfigList.java
@@ -1,10 +1,12 @@
 package com.example.projectscandium;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.projectscandium.model.ConfigManager;
 import com.example.projectscandium.model.Configs;
@@ -15,11 +17,18 @@ import java.util.ArrayList;
 public class GameConfigList extends AppCompatActivity {
 
     ListView ConfigList;
+    TextView configEmptyState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_config_list);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // set the title of the activity
+        setTitle("Game Configurations");
 
         // get the game config manager
         ConfigManager.getInstance().loadConfigs(this);
@@ -29,7 +38,13 @@ public class GameConfigList extends AppCompatActivity {
         newConfig.setOnClickListener(view -> {
             // create a new intent to start the new game activity
             Intent intent = new Intent(GameConfigList.this, GameConfig.class);
+            intent.putExtra("mode", "NewConfig");
             startActivity(intent);
+        });
+
+        // look for back button press
+        toolbar.setNavigationOnClickListener(v -> {
+            // do nothing
         });
     }
 
@@ -49,9 +64,12 @@ public class GameConfigList extends AppCompatActivity {
         // get the size of the array list
         int size = configManager.getSize();
 
+        configEmptyState = findViewById(R.id.configTutorial);
         if (size == 0) {
             ConfigList.setAdapter(null);
+            configEmptyState.setVisibility(TextView.VISIBLE);
         } else {
+            configEmptyState.setVisibility(TextView.GONE);
             // set the adapter for the list view
             ConfigListAdapter adapter = new ConfigListAdapter(this, R.layout.config_list_layout, configs);
             ConfigList.setAdapter(adapter);
@@ -62,6 +80,7 @@ public class GameConfigList extends AppCompatActivity {
                 Intent intent = new Intent(GameConfigList.this, GameConfig.class);
                 // pass the position of the config to the new activity
                 intent.putExtra("configIndex", position);
+                intent.putExtra("mode", "EditConfig");
                 startActivity(intent);
             });
         }
