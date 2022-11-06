1b6ad0b6c408dc4f7a2fc90cf8ba1217fbfe9ee5
Yixuan Liu <yla357@sfu.ca>
2022-11-05 19:45:28 -0700

    modify the combination

--- ProjectScandium/app/src/main/java/com/example/projectscandium/AddGame.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/AddGame.java
@@ -3,6 +3,7 @@ package com.example.projectscandium;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -22,13 +23,13 @@ import com.example.projectscandium.model.Game;
 
 public class AddGame extends AppCompatActivity {
 
-    private EditText etPlayer, etScore;
     private int players, scores;
-    private int gamePos, configPos;
+    private int gamePos;
+    private int configPos;
     Button btnDelete, btnSave;
 
     //Singleton the game list
-    private ConfigManager cm = ConfigManager.getInstance();
+    private final ConfigManager cm = ConfigManager.getInstance();
     private Configs config;
     private static final String CONFIG_POS = "com.example.projectscandium.AddGame - the config pos";
     private static final String GAME_POS = "com.example.projectscandium.AddGame - the gamePos";
@@ -38,14 +39,19 @@ public class AddGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_game);
 
+        // get config and game data
         extractDataFromIntent();
-        config = cm.getConfigById(configPos);
-        setTitle("Add Game");
-        if(gamePos != -1){setTitle(getString(R.string.editGame_title));}
 
-        //Enable return btn
-        ActionBar ab = getSupportActionBar();
-        ab.setDisplayHomeAsUpEnabled(true);
+        // get config instance
+        config = cm.getConfigById(configPos);
+
+        // set title
+        setTitle("Game: " + (config.getGameNum()+1));
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if(gamePos != -1){setTitle(getString(R.string.editGame_title));}
 
         setupPage();
 
@@ -69,21 +75,18 @@ public class AddGame extends AppCompatActivity {
     //App Bar Controller, setup function of save and return btn
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()){
-            //setup return button
-            case android.R.id.home: {
-                checkReturn();
-            }
+        //setup return button
+        if (item.getItemId() == android.R.id.home) {
+            checkReturn();
             return true;
-            default:
-                return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onBackPressed() {
         checkReturn();
-        super.onBackPressed();
+//        super.onBackPressed();
     }
 
     private void checkReturn(){
@@ -91,32 +94,22 @@ public class AddGame extends AppCompatActivity {
         builder1.setIcon(null);
         builder1.setTitle("Return to Game List?");
         builder1.setMessage("Nothing is saved");
-        builder1.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        });
-        builder1.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        }).show();
-    };
+        builder1.setPositiveButton("Yes", (dialog, which) -> finish());
+        builder1.setNegativeButton("No", (dialog, which) -> dialog.dismiss()).show();
+    }
 
     // set up Game Page
     private void setupPage() {
-        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnDelete = findViewById(R.id.btnDelete);
         if(gamePos == -1){
             btnDelete.setVisibility(View.GONE);
         }else{
             btnDelete.setVisibility(View.VISIBLE);
             Game game = config.getGames().get(gamePos);
-            etPlayer = (EditText) findViewById(R.id.player);
-            etScore = (EditText) findViewById(R.id.score);
-            etPlayer.setText("" + game.getPlayerNum());
-            etScore.setText("" + game.getCombinedScore());
+            EditText etPlayer = findViewById(R.id.player);
+            EditText etScore = findViewById(R.id.score);
+            etPlayer.setText(getString(R.string.addPlayer, game.getPlayerNum()));
+            etScore.setText(getString(R.string.addScore, game.getCombinedScore()));
             setupDeleteBtn();
         }
         setupSaveBtn();
@@ -124,49 +117,35 @@ public class AddGame extends AppCompatActivity {
 
     // set up delete button
     private void setupDeleteBtn() {
-        btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //create dialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(AddGame.this);
-                builder.setIcon(null);
-                builder.setTitle("Delete?");
-                builder.setMessage("Are You Sure Want to Delete This Game?");
-                //make Sure
-                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        finish();
-                        config.getGames().remove(gamePos);
-                    }
-                });
-                //Cancel
-                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                }).show();
-            }
+        btnDelete.setOnClickListener(view -> {
+            //create dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(AddGame.this);
+            builder.setIcon(null);
+            builder.setTitle("Delete?");
+            builder.setMessage("Are You Sure Want to Delete This Game?");
+            //make Sure
+            builder.setPositiveButton("Yes", (dialogInterface, i) -> {
+                finish();
+                config.getGames().remove(gamePos);
+            });
+            //Cancel
+            builder.setNegativeButton("No", (dialogInterface, i) -> dialogInterface.dismiss()).show();
         });
     }
 
     // set up save button
     private void setupSaveBtn() {
         btnSave = findViewById(R.id.btnSave);
-        btnSave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(checkValid()){
-                    //Save data, calculate level and save into class
-                    String sPlayer = ((String)((EditText)findViewById(R.id.player)).getText().toString());
-                    players = Integer.parseInt(sPlayer);
-                    String sScore = ((String)((EditText)findViewById(R.id.score)).getText().toString());
-                    scores = Integer.parseInt(sScore);
-                    checkSave();
-                    Game game = new Game(players, scores);
-                    config.addGame(game);
-                }
+        btnSave.setOnClickListener(v -> {
+            if(checkValid()){
+                //Save data, calculate level and save into class
+                String sPlayer = ((EditText)findViewById(R.id.player)).getText().toString();
+                players = Integer.parseInt(sPlayer);
+                String sScore = ((EditText)findViewById(R.id.score)).getText().toString();
+                scores = Integer.parseInt(sScore);
+                checkSave();
+                Game game = new Game(players, scores);
+                config.addGame(game);
             }
         });
     }
@@ -176,34 +155,28 @@ public class AddGame extends AppCompatActivity {
         builder.setIcon(null);
         builder.setTitle("Save Game?");
         builder.setMessage("Game will be saved.");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if(gamePos == -1) {
-                    Game newGame = new Game(players, scores);
-                    config.getGames().add(newGame);
-                }else {
-                    config.getGames().get(gamePos).restoreGame(players, scores);
-                }
-                finish();
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            if(gamePos == -1) {
+                Game newGame = new Game(players, scores);
+                config.getGames().add(newGame);
+            }else {
+                config.getGames().get(gamePos).restoreGame(players, scores);
             }
+            finish();
         });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        }).show();
+        builder.setNegativeButton("No", (dialog, which) -> dialog.dismiss()).show();
     }
 
     //Check if the game is valid
     private boolean checkValid() {
-        if(etPlayer.length() == 0){
+        EditText etP =findViewById(R.id.player);
+        EditText etS = findViewById(R.id.score);
+        if(etP.length() == 0){
             Toast.makeText(AddGame.this, "Players should not be empty",
                     Toast.LENGTH_LONG).show();
             return false;
         }
-        if(etScore.length() == 0){
+        if(etS.length() == 0){
             Toast.makeText(AddGame.this, "Scores should not be empty",
                     Toast.LENGTH_LONG).show();
             return false;
--- ProjectScandium/app/src/main/java/com/example/projectscandium/GameList.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/GameList.java
@@ -19,17 +19,17 @@ import com.example.projectscandium.model.Game;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class GameList extends AppCompatActivity {
     //Singleton the game list
-    private ConfigManager cm = ConfigManager.getInstance();
+    private final ConfigManager cm = ConfigManager.getInstance();
     //Setup local game list
-    private ArrayList<Game> games = new ArrayList<>();
+    private final ArrayList<Game> games = new ArrayList<>();
     private ArrayAdapter<Game> adapter;
 
     private int configPos;
     private static final String CONFIG_POS = "com.example.projectscandium.GameList - the Config position";
-    private Configs config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +53,7 @@ public class GameList extends AppCompatActivity {
         super.onResume();
         games.clear();
         populateGameList();
+        populateListView();
         adapter.notifyDataSetChanged();
     }
 
@@ -60,22 +61,23 @@ public class GameList extends AppCompatActivity {
     private void extractDataFromIntent() {
         Intent intent = getIntent();
         configPos = intent.getIntExtra(CONFIG_POS, 0);
-        config = cm.getConfigById(configPos);
+        Configs config = cm.getConfigById(configPos);
         String configName = config.getGameConfigName();
-        getSupportActionBar().setTitle("Configuration: " + configName);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Configuration: " + configName);
     }
 
     // get the game list from singleton
     private void populateGameList() {
         Configs config = cm.getConfigById(configPos);
+        TextView txtEmpty = findViewById(R.id.txtEmpty);
         if(config.getGameNum() == 0){
-            // empty state...
+            txtEmpty.setVisibility(View.VISIBLE);
         }else{
+            txtEmpty.setVisibility(View.GONE);
             for(int i = 0; i < config.getGameNum(); i++){
                 games.add(config.searchGame(i));
             }
         }
-
     }
 
     // create list view
@@ -99,7 +101,7 @@ public class GameList extends AppCompatActivity {
 
             // set game num txt
             TextView txtNum = itemView.findViewById(R.id.GameNum);
-            txtNum.setText(getString(R.string.game_num, position));
+            txtNum.setText(getString(R.string.game_num, position+1));
 
             // Set Player txt
             TextView txtPlayer = itemView.findViewById(R.id.txtPlayer);
@@ -109,6 +111,9 @@ public class GameList extends AppCompatActivity {
             TextView txtScore = itemView.findViewById(R.id.txtScore);
             txtScore.setText(getString(R.string.game_score, currentGame.getCombinedScore()));
 
+            TextView txtLevel = itemView.findViewById(R.id.txtLevel);
+            txtLevel.setText(getString(R.string.game_level, 0));
+
             return itemView;
         }
     }
@@ -116,12 +121,9 @@ public class GameList extends AppCompatActivity {
     //Set up switch activity for click game
     private void registerCLickCallback() {
         ListView list = findViewById(R.id.gameList);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View viewClicked, int position, long id) {
-                Intent intent = AddGame.makeIntent(GameList.this, configPos, position);
-                startActivity(intent);
-            }
+        list.setOnItemClickListener((parent, viewClicked, position, id) -> {
+//                Intent intent = AddGame.makeIntent(GameList.this, configPos, position);
+//                startActivity(intent);
         });
     }
 
@@ -132,12 +134,9 @@ public class GameList extends AppCompatActivity {
 
     private void setupAddGameBtn() {
         FloatingActionButton btn = findViewById(R.id.addGamebtn);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = AddGame.makeIntent(GameList.this, configPos, -1);
-                startActivity(intent);
-            }
+        btn.setOnClickListener(v -> {
+            Intent intent = AddGame.makeIntent(GameList.this, configPos, -1);
+            startActivity(intent);
         });
     }
 
