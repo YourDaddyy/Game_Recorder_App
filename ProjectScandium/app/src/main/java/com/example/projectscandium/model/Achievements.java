a6d2927d9c23d0bc157cb4c223173cc994029cc7
Yixuan Liu <yla357@sfu.ca>
2022-11-06 16:26:47 -0800

    Finished Activity with Achievements
    Implemented the achievement with game list. Modify the game list layout, adding features to addGame

--- ProjectScandium/app/src/main/java/com/example/projectscandium/model/Achievements.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/model/Achievements.java
@@ -11,7 +11,7 @@ public class Achievements {
             "Silly Cat", "Ktten Army", "Flabbergast Cat", "Nyan Kitty", "Aye Aye Cat-tain" };
 
     // aray of int values for each achievement
-    private int[] achievementValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    private final int[] achievementValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     private int lowerScoreBound;
     private int upperScoreBound;
@@ -74,4 +74,21 @@ public class Achievements {
         return achievements[9];
     }
 
+    public int getAchievementIndex(int score) {
+        if (score < lowerScoreBound) {
+            return 0;
+        }
+        // if the score is greater than the upper bound, return the last achievement
+        if (score > upperScoreBound) {
+            return 9;
+        }
+        // if the score is between the lower and upper bound, return the achievement
+        // that corresponds to the score
+        for (int i = 0; i < 10; i++) {
+            if (score < achievementValues[i]) {
+                return i;
+            }
+        }
+        return 9;
+    }
 }
