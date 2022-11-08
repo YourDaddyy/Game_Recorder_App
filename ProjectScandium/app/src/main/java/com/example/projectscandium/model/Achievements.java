62eee2febd935401061e9e5e1bd544596c7cd2d8
ekw11 <ekw11@sfu.ca>
2022-11-07 16:18:20 -0800

    Debugging lvl issues
    Solving a bug that displays higher achievement lvls. Passing it over to ethan

--- ProjectScandium/app/src/main/java/com/example/projectscandium/model/Achievements.java
+++ ProjectScandium/app/src/main/java/com/example/projectscandium/model/Achievements.java
@@ -10,7 +10,7 @@ public class Achievements {
             "Silly Cat", "Kitten Army", "Flabbergast Cat", "Nyan Kitty", "Aye Aye Cat-tain" };
 
     // array of int values for each achievement
-    private final int[] achievementValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    private final double[] achievementValues = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
     // variable to store the lower bound and upper bound of the achievement scores
     private int lowerScoreBound;
@@ -26,14 +26,14 @@ public class Achievements {
     // getAchievementValue
     // Purpose: returns the value of the achievement at the given index
     // Return: int
-    public int getAchievementValue(int index) {
+    public double getAchievementValue(int index) {
         return achievementValues[index];
     }
 
     // setAchievementValue
     // Purpose: sets the value of the achievement at the given index
     // Return: void
-    public void setAchievementValue(int index, int value) {
+    public void setAchievementValue(int index, double value) {
         this.achievementValues[index] = value;
     }
 
@@ -41,21 +41,22 @@ public class Achievements {
     // Purpose: returns the lower bound of the achievement scores
     // Return: int
     public void setScoreBounds(int lower, int upper, int numPlayers) {
+
+        // set worse score possible
+        setAchievementValue(0, 0);
+
         this.lowerScoreBound = lower * numPlayers;
         this.upperScoreBound = upper * numPlayers;
 
-        // Create worse possible lvl which always equals 0 points
-        // A score to include the worse possible games (lower than lowerScoreBound)
-        int worsePossibleScore = 0;
-        setAchievementValue(0, worsePossibleScore);
-
-        int range = this.upperScoreBound - this.lowerScoreBound;
-        int increment = range / (9 - 1);
+        int range = upperScoreBound - lowerScoreBound;
+        double increment = (double)range / (9 - 1);
         for (int i = 1; i < 10; i++) {
-            int points = this.lowerScoreBound + (increment * (i - 1));
+            double points = lowerScoreBound + (increment * (i - 1));
             setAchievementValue(i, points);
         }
     }
+
+
 
     // getLowerScoreBound
     // Purpose: returns the lower bound of the achievement scores
@@ -75,11 +76,13 @@ public class Achievements {
     // Purpose: returns the achievement name based on the score
     // Return: String
     public String getAchievement(int score) {
-        if (score < lowerScoreBound) {
+        double doubleScore = score;
+
+        if (doubleScore < (double)lowerScoreBound) {
             return achievements[0];
         }
         // if the score is greater than the upper bound, return the last achievement
-        if (score > upperScoreBound) {
+        if (doubleScore > (double)upperScoreBound) {
             return achievements[9];
         }
         // if the score is between the lower and upper bound, return the achievement
@@ -96,17 +99,21 @@ public class Achievements {
     // Purpose: returns the index of the achievement that the given score belongs to
     // Return: int
     public int getAchievementIndex(int score) {
-        if (score < lowerScoreBound) {
+        double doubleScore = (double)score;
+        double doubleLower = lowerScoreBound;
+        double doubleUpper = upperScoreBound;
+
+        if (doubleScore < doubleLower) {
             return 0;
         }
         // if the score is greater than the upper bound, return the last achievement
-        if (score > upperScoreBound) {
+        if (doubleScore > doubleUpper) {
             return 9;
         }
         // if the score is between the lower and upper bound, return the achievement
         // that corresponds to the score
         for (int i = 0; i < 10; i++) {
-            if (score < achievementValues[i]) {
+            if (doubleScore < achievementValues[i]) {
                 return i;
             }
         }
