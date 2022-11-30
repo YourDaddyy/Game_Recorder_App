3edba20a365e918e892c3663590e22fdfcc8a41c
Sahaj Singh <ssa365@sfu.ca>
2022-11-30 01:46:02 -0800

    Added Hyperlinks to the resources used in the About Us Section
    Signed-off-by: SahajS <ssa365@sfu.ca>

--- app/src/main/java/com/example/projectscandium/AboutPage.java
+++ app/src/main/java/com/example/projectscandium/AboutPage.java
@@ -5,11 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Gravity;
+import android.widget.TextView;
 
 import java.util.Objects;
 
+/*Class AboutPage
+ * Purpose: This class is the activity that allows the user to view the About Us page
+ */
 public class AboutPage extends AppCompatActivity {
 
+    // onCreate method
+    // Purpose: creates the activity, set the toolbar (including the title).
+    // Loads the list of configs from the config manager(shared preferences)
+    // Returns: void
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +34,21 @@ public class AboutPage extends AppCompatActivity {
 
         // set the title of the activity
         setTitle(R.string.aboutPageTitle);
+
+        TextView aboutPageText = findViewById(R.id.Resources_Text);
+        aboutPageText.setGravity(Gravity.CENTER);
+        aboutPageText.setMovementMethod(LinkMovementMethod.getInstance());
+        aboutPageText.setText(Html.fromHtml("<b>Tutorials</b> <br> " +
+                "- Dr Brian's YouTube Tutorials <br>" +
+                "<a href=https://www.youtube.com/watch?v=KsyCfxxEIwQ>- Gif Animation Tutorial</a><br>"+
+                "<a href=https://www.youtube.com/watch?v=0SJAK_pKUoE>- Sound Tutorial <br>" +
+                "<a href=https://devstudioonline.com/article/create-rounded-background-as-border-radius-in-android-layout>- Rounded Buttons </a><br>" +
+                "<a href=https://www.youtube.com/watch?v=5PaWtQAOdi8>- Setup custom Alert Dialog </a><br> <br>" +
+
+                "<b>Media</b> <br>" +
+                "<a href=https://sciencenotes.org/scandium-facts/>-Scandium Icon Source</a><br>"+
+                "<a href=https://www.freepik.com/free-vector/illustration-animal-adult-coloring-page_2614622.htm#query=cat%20art&position=2&from_view=search&track=sph>-Game Image Source</a><br>"
+                ,Html.FROM_HTML_MODE_LEGACY));
     }
 
     // onBackPressed method
